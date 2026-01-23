3e60a371134d2e18bbfaf1b1751c1910ba546bdf
TuHiN HoQuE <104512309+tuhinhoq@users.noreply.github.com>
2026-01-23 21:16:06 +0600

    Create Day_4.py
    Important

---
+++ Day_4.py
@@ -0,0 +1,29 @@
+#Creating String
+letter='T'
+print(letter)
+print(len(letter))
+
+greeting="I hope you are well"
+print(len(greeting))
+
+greetings='''Hi Iam Tuhin
+from Dhaka'''
+print(greetings)
+
+
+first_name='Tuhin'
+last_name='Hoque'
+space=' '
+age=36
+flt=3.165487
+
+print(first_name+space+last_name)
+print(len(first_name+space+last_name))
+
+
+print('I am Tuhin.\n I love \\programming \t ever')
+print('Date \t Month \t Year')
+print('01 \t 01 \t 2026')
+print('------------------------------------------')
+
+print('I am %s %s & %d years old. GPA: %.2f'%(first_name,last_name,age,flt))  # <--- Very Important
