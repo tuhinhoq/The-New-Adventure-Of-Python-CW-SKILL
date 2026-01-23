093d5a6b284805038cce0a851fb126e09f09ebc4
TuHiN HoQuE <104512309+tuhinhoq@users.noreply.github.com>
2026-01-23 23:57:30 +0600

    List_Details

---
+++ Day_5.py
@@ -0,0 +1,24 @@
+#List
+
+persons=['Karim', 'Rahim', 'Abdul','Rabbi', 'Shakil']
+age=[22,33,44,55,66]
+mix_data=['Tuhin', 35, {'Address':'Dhaka', 'Skill':'Python'}]
+
+w,x,*rest=mix_data
+print(persons)
+print(len(persons))
+print(age)
+print(len(age))
+print(mix_data)
+print(type(mix_data))
+print(len(mix_data))
+print(mix_data.pop(0))
+print(mix_data)
+
+num_r=[1,2,3,7,5,6,4]
+pr=sorted(num_r)
+print(pr)
+
+
+
+
