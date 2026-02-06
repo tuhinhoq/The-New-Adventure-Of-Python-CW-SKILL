c499ece04d1ab26940b1a3ed0f11bfeb54e915bb
TuHiN HoQuE <104512309+tuhinhoq@users.noreply.github.com>
2026-02-06 20:23:16 +0600

    Day_9

---
+++ Day_9.py
@@ -0,0 +1,33 @@
+a=8
+
+if a%2==0:
+    print("Even")
+    
+else:
+    print("Odd")
+    
+    
+    
+    
+        
+day=int(input("Enter a Number: "))
+
+if day==1: print("Today Is Saturday")
+elif day==2: print("Today Is Sunday")
+elif day==3: print("Today Is Monday")
+elif day==4: print("Today Is Tuesday")
+elif day==5: print("Today Is Wednesday")
+elif day==6: print("Today Is Thursday")
+elif day==7: print("Today Is Weekend!!")
+else: print("Invalid Input!! Enter 1-7...") 
+
+a = -8
+if a > 0:
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')
